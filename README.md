fc9eabf2a609572ba238cb21a8c07a1ab02e2c1f
ET-CS <et.programming@gmail.com>
2012-06-28 14:05:42 +0300

    modules.php is_dir

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ PHP Framework to manage your code as plug & play modules with admin panel
 for easy maintaince of your modules.
 no modules provided with this base framework.
 
-you can watch a demo site created using BR-Framework @ http://etcs.hopto.org/
+you can watch a demo site created based on BR-Framework @ http://etcs.hopto.org/
 
 v0.01
--- sys/modules.php
+++ sys/modules.php
@@ -1,8 +1,8 @@
 <?php
     $pluginpath = '';
     function includeModules($path, $level) {
-        global $pluginpath;
-        if ($handle = opendir($path)) {            
+        global $pluginpath;        
+        if ($handle = opendir($path)) {
             /* loop over the directory. */
             while (false !== ($entry = readdir($handle))) {
                 if ($entry != "." && $entry != "..") {
@@ -14,8 +14,8 @@
                         $page = file_get_contents($path.$entry);
                         $pos = strpos($page, "/*");
                         $pos2 = strpos($page, "*/");
-                        $part = substr($page, $pos, $pos2);                                
-                        if (strpos($part, "Plugin Name:")) {                            
+                        $part = substr($page, $pos, $pos2);
+                        if (strpos($part, "Plugin Name:")) {
                             $pluginpath = $path;
                             include $path.$entry;
                         }                                
@@ -25,7 +25,11 @@
             closedir($handle);
             $pluginpath = '';
         }
-    }            
-    includeModules(DIR_SYSTEM_MODULES, 1);
-    includeModules(DIR_MODULES, 1);
+    }
+    if (is_dir(DIR_SYSTEM_MODULES)) {
+        includeModules(DIR_SYSTEM_MODULES, 1);
+    }
+    if (is_dir(DIR_MODULES)) {
+        includeModules(DIR_MODULES, 1);
+    }
 ?>
