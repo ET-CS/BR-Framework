06e78f9eded2d3cc4ec1471d979c0dc141e5fe74
ET-CS <et.programming@gmail.com>
2012-07-10 05:32:40 +0300

    empty template for html5 using br-framework

---
+++ public/html5base.php
@@ -0,0 +1,15 @@
+<?php include "bootstrap.php"; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>        
+    </head>
+    <body>
+        <section>
+            <header><h1>Empty Page</h1></header>
+            <nav></nav>
+        </section>
+        <aside></aside>
+        <footer></footer>
+    </body>
+</html>
