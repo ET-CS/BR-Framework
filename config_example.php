a879795c13dbe87215674a744ff560470bc5a54b
ET-CS <et.programming@gmail.com>
2013-06-24 17:33:23 +0300

    config_example updated. modules skin manager moved out as plugin

--- config_example.php
+++ config_example.php
@@ -1,6 +1,9 @@
 <?php
     # Example config.php file
 
+    # DB mode.
+    # None = Use filesystem to store data.
+    define("DB_TYPE", "None");
     # scanning depth of modules directory.
     # change to this value is not recommended unless you know what it's mean
     define("MODULES_SCAN_LEVEL", "1");
--- sys/modules.php
+++ sys/modules.php
@@ -1,5 +1,4 @@
 <?php
-
     /* TODO add option/module to cache modules (For timelapse/until renew/etc)  */
 
     $pluginpath = '';
@@ -8,12 +7,14 @@
         if ($handle = opendir($path)) {
             /* loop over the directory. */
             while (false !== ($entry = readdir($handle))) {
+                #echo 'Checking: '.$path.$entry.'</br>';            
                 if ($entry != "." && $entry != "..") {
                     if (is_dir($path.$entry) === true){
-                        //currently not scanning modules deeper. uncomment for recursive scanning
+                        //currently not scanning moduleGs deeper. uncomment for recursive scanning
                         if ($level < MODULES_SCAN_LEVEL+1)
                             includeModules($path.$entry."/", $level+1);
                     }else{
+                        #echo 'Loading: '.$path.$entry.'</br>';
                         $page = file_get_contents($path.$entry);
                         $pos = strpos($page, "/*");
                         $pos2 = strpos($page, "*/");
@@ -21,7 +22,7 @@
                         if (strpos($part, "Plugin Name:")) {
                             $pluginpath = $path;
                             include $path.$entry;
-                            #echo $entry.' loaded.';
+                            #echo 'plugin '.$path.$entry.' loaded.<br/>';
                         }                                
                     }
                 }
